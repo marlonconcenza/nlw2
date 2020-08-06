f2742f4f00eee8053b5be676fb8ac2fdaf7d3be7
marlonconcenza <marlonconcenza@gmail.com>
2020-08-06 16:37:13 -0300

    Add axios

--- mobile/src/pages/Landing/index.tsx
+++ mobile/src/pages/Landing/index.tsx
@@ -1,18 +1,22 @@
-import React from 'react';
-import { View, Image, Text, TouchableOpacity } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, Image, Text } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { RectButton } from 'react-native-gesture-handler';
-
-import styles from './styles';
 
 import landingImg from '../../assets/images/landing.png';
 import studyIcon from '../../assets/images/icons//study.png';
 import giveClassesIcon from '../../assets/images/icons/give-classes.png';
 import heartIcon from '../../assets/images/icons/heart.png';
 
+import api from '../../services/api';
+
+import styles from './styles';
+
 const Landing = () => {
 
     const { navigate } = useNavigation();
+
+    const [ totalConnections, setTotalConnections ] = useState(0);
 
     function handleNavigateToGiveClassesPage() {
         navigate('GiveClasses');
@@ -21,6 +25,17 @@ const Landing = () => {
     function handleNavigateToStudyPages() {
         navigate('Study');
     }
+
+    useEffect(() => {
+        api.get('/connections')
+            .then(response => {
+                const { total } = response.data;
+                setTotalConnections(total);
+            })
+            .catch(error => {
+                console.log('error', error);
+            });
+    }, []);
 
     return (
         <View style={styles.container}>
@@ -44,7 +59,7 @@ const Landing = () => {
             </View>
 
             <Text style={styles.totalConnections}>
-                Total de 285 conexões já realidas {' '}
+                Total de {totalConnections} conexões já realidas {' '}
                 <Image source={heartIcon} />
             </Text>
         </View>
