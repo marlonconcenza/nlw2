957fb2d58a37bf517e80d776e66ef8ed3a77351c
marlonconcenza <marlonconcenza@gmail.com>
2020-08-06 15:20:49 -0300

    First commit

---
+++ mobile/src/pages/Favorites/styles.ts
@@ -0,0 +1,13 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#f0f0f7",
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center'
+    }
+});
+
+export default styles;
---
+++ mobile/src/pages/TeacherList/styles.ts
@@ -0,0 +1,14 @@
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "#f0f0f7",
+        justifyContent: "center",
+    },
+    content: {
+        flex: 1,
+        justifyContent: 'center'
+    }
+});
+
+export default styles;
